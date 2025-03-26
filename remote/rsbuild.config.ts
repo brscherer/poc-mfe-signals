3cc12a0f33182359b788a0167fc492ee83b2a02a
Bruno Scherer <bruno.raphael96@gmail.com>
2025-03-26 10:23:21 -0300

    exposes counter

--- remote/rsbuild.config.ts
+++ remote/rsbuild.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
+
 
 export default defineConfig({
-  plugins: [pluginReact()],
+  plugins: [
+    pluginReact(),
+    pluginModuleFederation({
+      name: "remote_app",
+      exposes: {
+        "./Counter": "./src/components/Counter",
+      },
+      // shared: ["react", "react-dom"],
+    })
+  ],
 });
